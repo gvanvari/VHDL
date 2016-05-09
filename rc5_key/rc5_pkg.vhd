26ad97cca541d61d301aac8f2452f51d0a84fc7b
gvanvari <gvanvari@gmail.com>
2016-05-09 18:02:55 -0400

    Revert "RC5 Encryption and Decryption"
    This reverts commit 5db9c1f22897079dcd692c9cfe1092f07a7a6069.

--- rc5_key/rc5_pkg.vhd
+++
@@ -1,8 +0,0 @@
-LIBRARY	IEEE;
-USE	IEEE.STD_LOGIC_1164.ALL;
-
-PACKAGE rc5_pkg IS
-	type rom_s is array (0 to 25) of std_logic_vector(31 downto 0);
-	type rom_l is array (0 to 3) of std_logic_vector(31 downto 0);
-	type rom_ukey is array (0 to 3) of std_logic_vector(127 downto 0);
-END rc5_pkg;
